d445fbd90ffc5014e77737a26c3231d4c121c1e4
Tash Zaman <ztash2000@gmail.com>
2023-03-22 16:04:38 -0300

    Added some additional spacing to my box

--- frontend/src/pages/Admin/ViewPet.jsx
+++ frontend/src/pages/Admin/ViewPet.jsx
@@ -38,7 +38,7 @@ function ViewPet(){
         <div className="login-header padding-top-128">
         <div className="pet-info-box">
         <h1 className="padding-bottom-16"><b>View The Client's Pet Information</b></h1>
-          <div className="UserTable" style={{marginBottom: "800px" }}>
+          <div className="UserTable" style={{marginBottom: "750px" }}>
               {results.map((pet) => {
                 return(
                   <div key={pet.pet_id}>
