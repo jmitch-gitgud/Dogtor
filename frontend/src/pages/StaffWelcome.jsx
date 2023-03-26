93e0b2554b5582d215b3250f1c6bcd457a1944cc
jmitch-gitgud <80692859+jmitch-gitgud@users.noreply.github.com>
2023-03-26 11:05:28 -0300

    Took out the text under the photo links, added a new photo for the grooming ad

--- frontend/src/pages/StaffWelcome.jsx
+++ frontend/src/pages/StaffWelcome.jsx
@@ -1,6 +1,6 @@
 import StaffNavBar from "../components/StaffNavBar";
 import dunRoaminImg from './images/DunRoamin.png';
-import groomingAdImg from './images/GroomingAd.jpg'
+import groomingAdImg from './images/GroomingAd.png'
 import dozenRosesImg from './images/ADozenRoses.png'
 
 import React from "react";
@@ -26,12 +26,6 @@ function welcome() {
             <nav style={{ margin: "auto", marginBottom: "30px"}}>
             <a href="https://www.facebook.com/profile.php?id=100081835885169"><img className="welcome-photo" alt="DozenRoses_icon" src={dozenRosesImg} width="400" height="300"/></a>
             </nav>
-            </div>
-
-            <div className="welcome-labels">
-              <div className="welcome-label">Adopt a new friend or make a donation to DunRoamin!</div>
-              <div className="welcome-label grooming-label">Is your pet ready for a fresh new look?</div>
-              <div className="welcome-label dozen-label">Find a fur-ever friend or make a donation to A Dozen Roses!</div>
             </div>
         </div>
         </div>
